8fe672a4b503380eec7f77342600f3cd60b016ad
ruth610 <106428054+ruth610@users.noreply.github.com>
2025-03-06 14:43:27 +0300

    modified

--- amazon-clone/vite.config.js
+++ amazon-clone/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+
+  ],
+  base: './', // or `/Amazon-Clone-2025/` if using a repo-based GitHub Page
 })
